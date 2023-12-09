80a623e2cc7dd35d64e49b2ce52fdc04bd87ce73
Kevin Kleinegger <kevinkleinegger@kevins-air.myfiosgateway.com>
2023-12-09 13:30:09 -0500

    day 5 complete

--- day5.py
+++ day5.py
@@ -1,44 +1,69 @@
-import re
-import sys
-from tqdm import tqdm
-
 def main():
-    with open("input.txt", 'r') as file:
-        input_str = file.read()
-        min_v = sys.maxsize
-        seeds = [int(x) for x in re.search("seeds:.*?\n", input_str, re.DOTALL).group(0).replace('seeds: ', '').strip().split(' ')]
-        for i in range(0, len(seeds), 2):
-            for j in tqdm(range(seeds[i], seeds[i]+seeds[i+1])):
-                value = extract_location(input_str, j)
-                if(value < min_v):
-                    min_v = value
-        print(min_v)
-       
-def extract_location(s_in, seed):
-    s2s_map = re.search("seed-to-soil map:.*?soil-to-fertilizer map:", s_in, re.DOTALL).group(0)
-    s2f_map = re.search("soil-to-fertilizer map:.*?fertilizer-to-water map:", s_in, re.DOTALL).group(0)
-    f2w_map = re.search("fertilizer-to-water map:.*?water-to-light map:", s_in, re.DOTALL).group(0)
-    w2l_map = re.search("water-to-light map:.*?light-to-temperature map:", s_in, re.DOTALL).group(0)
-    l2t_map = re.search("light-to-temperature map:.*?temperature-to-humidity map:", s_in, re.DOTALL).group(0)
-    t2h_map = re.search("temperature-to-humidity map:.*?humidity-to-location map:", s_in, re.DOTALL).group(0)
-    h2l_map = re.search("humidity-to-location map:.*?$", s_in, re.DOTALL).group(0)
-    soil = map_value(s2s_map, seed)
-    fert = map_value(s2f_map, soil)
-    water = map_value(f2w_map, fert)
-    light = map_value(w2l_map, water)
-    temp = map_value(l2t_map, light)
-    hum = map_value(t2h_map, temp)
-    return map_value(h2l_map, hum)
-       
+    with open("day5.txt", 'r') as file:
+        input = file.read().strip()
+        print(part1(input))
+        print(part2(input))
 
-def map_value(m, i):
-    m = list(m.split('\n'))
-    for line in m:
-        if len(line) > 0 and line[0].isnumeric():
-            d, s, r = [int(x) for x in line.split(' ')]
-            if i >= s and i <= s+r:
-                return i + (d-s)
-    return i
+def get_maps(maps):
+    all_maps = []
+    for m in maps:
+        m = m.split('\n')[1:]
+        m = [[int(x) for x in i.split(' ')] for i in m]
+        all_maps.append(m)
+    return all_maps
+
+def apply_map(seed, map):
+    for d, s, r in map:
+        if seed >= s and seed <= s+r:
+            return seed + (d-s)
+    return seed
+
+def apply_range(ranges, map):
+    final = []
+    for d, s, r in map:
+        need_work = []
+        src_end = s + r
+        while(ranges):
+            (start, end) = ranges.pop()
+            before = (start, min(s, end))
+            inner = (max(start,s), min(src_end, end))
+            after = (max(src_end, start), end)
+            if(before[1] > before[0]):
+                need_work.append(before)
+            if(inner[1] > inner[0]):
+                final.append((inner[0]-s+d, inner[1]-s+d))
+            if(after[1] > after[0]):
+                need_work.append(after)
+        ranges = need_work
+    return final+ranges
+
+def apply_maps(seed, maps):
+    new_val = seed
+    for m in maps:
+        new_val = apply_map(new_val, m)
+    return new_val
+
+def part1(input):
+    seeds, *maps = input.split('\n\n')
+    seeds = [int(x) for x in seeds.split(':')[1].split()]
+    maps = get_maps(maps)
+    locations = []
+    for s in seeds:
+        locations.append(apply_maps(s, maps))
+    return min(locations)
+
+def part2(input):
+    seeds, *maps = input.split('\n\n')
+    seeds = [int(x) for x in seeds.split(':')[1].split()]
+    maps = get_maps(maps)
+    seed_pairs = list(zip(seeds[::2], seeds[1::2]))
+    outputs = []
+    for ss, ez in seed_pairs:
+        range = [(ss, ss+ez)]
+        for m in maps:
+            range = apply_range(range, m)
+        outputs.append(min(range)[0])
+    return min(outputs)
 
 if __name__ == "__main__":
-    main()      
+    main()
